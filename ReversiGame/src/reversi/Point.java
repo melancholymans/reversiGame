1bdce6d04c45e77a75fccf0fd95a40aa1bc3afbc
joshua <kjkq26993@gaia.eonet.ne.jp>
2016-10-30 05:32:26 +0900

    Pointクラス追加

---
+++ ReversiGame/src/reversi/Point.java
@@ -0,0 +1,16 @@
+package reversi;
+
+public class Point
+{
+	public Point()
+	{
+		this(0,0);
+	}
+	public Point(int x,int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	public int x;
+	public int y;
+}
